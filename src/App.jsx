c7d6a2234de669818a216bdb58947fcb96cfe231
jyotirmayapati <jyotirmayapati70@gmail.com>
2024-08-13 15:58:25 +0530

    feat - Addded new feature to - login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import AllRoutes from './AllRoutes/AllRoutes'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <AllRoutes/>
+      <AllRoutes />
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,70 +1,55 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import AuthContext from '../Context/AuthContext';
 
 const Navbar = () => {
+  const { isLoggedIn, logout } = useContext(AuthContext);
 
-    const Links = [
-        {
-            path: "/",
-            name: "Home"
-        },
-        {
-            path: "/cart:id",
-            name: "Cart"
-        },
-        {
-            path: "/contact",
-            name: "Contact"
-        },
-        {
-            path: "/login",
-            name: "Login"
-        },
-    ]
+  const Links = [
+    { 
+      path: "/", 
+      name: "Home" 
+    },
+    { 
+      path: "/cart:id", 
+      name: "Cart" 
+    },
+    { 
+      path: "/contact", 
+      name: "Contact" 
+    },
+    {
+      path: isLoggedIn ? "/" : "/login",
+      name: isLoggedIn ? "Logout" : "Login",
+      onClick: isLoggedIn ? logout : null,
+    },
+  ];
 
-    const active = {
-        color: "red",
-        textDecoration: 'none'
-    }
-    const inactive = {
-        color: 'black',
-        textDecoration: 'none'
-    }
+  const active = { color: "red", textDecoration: 'none' };
+  const inactive = { color: 'black', textDecoration: 'none' };
 
+  return (
+    <div className='navbar' 
+    style={{ display: 'flex', 
+             justifyContent: 'space-around', 
+             backgroundColor: 'lightblue', 
+             alignItems: 'center' 
+          }}>
+      <h3>EcomExpress</h3>
+      <div style={{ display: 'flex', gap: '90px' }}>
+        {Links.map((link) => (
+          <NavLink
+            key={link.name}
+            to={link.path}
+            style={({ isActive }) => (isActive ? active : inactive)}
+            onClick={link.onClick}
+          >
+            {link.name}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <>
-            <div className='navbar' style={{
-                display: 'flex',
-                justifyContent: 'space-around',
-                top: '0',
-                margin: '0',
-                padding: '0',
-                backgroundColor: 'lightblue',
-                alignItems: 'center',
-            }}>
-                <h3>EcomExpress</h3>
-                <div style={{
-                    display: 'flex',
-                    gap: '90px'
-                }}>
-                    {Links.map((el) => {
-                        return (
-                            <NavLink
-                                style={({ isActive }) => isActive ? active : inactive}
-                                // className={({ isActive }) => isActive ? 'active' : 'default'}
-                                to={el.path} key={el.name}>
-                                {el.name}
-                            </NavLink>
-                        )
-                    })
-                    }
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default Navbar
-
+export default Navbar;
